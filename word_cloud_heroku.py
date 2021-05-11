d6fb122d7cbd26891d6c67821deda622bdf229fa
Xinlong Dai <xinlong3@illinois.edu>
2021-05-11 10:37:28 -0500

    fix requirements

--- word_cloud_heroku.py
+++ word_cloud_heroku.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
 from main import keyword_extraction_text_rank
 
 st.title("Welcome to WordCloud!")
@@ -9,8 +8,4 @@ st.text("Put your text here and generate your word cloud!")
 user_input = st.text_area("text goes here")
 extracted_key_words, frequency_dict = keyword_extraction_text_rank(user_input)
 word_cloud = WordCloud(width = 800, height = 800, background_color ='white', min_font_size = 10).generate_from_frequencies(frequency_dict)
-# plt.figure(figsize = (8, 8), facecolor = None)
-# plt.imshow(word_cloud)
-# plt.axis("off")
-# plt.show()
 st.image(word_cloud)
